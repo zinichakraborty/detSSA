f879199ed711eca57ac7b85b6f2af999edfeb90d
zchakraborty <zinichakraborty@gmail.com>
2023-02-18 15:36:33 -0500

    start SSA classes

---
+++ src/SSA.java
@@ -0,0 +1,11 @@
+public class SSA<T extends Comparable<? super T>> {
+    private SSANode<T> root;
+    private int size;
+    public SSA() {
+        root = null;
+        size = 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
---
+++ src/SSANode.java
@@ -0,0 +1,5 @@
+public class SSANode<T> {
+    public SSANode() {
+        
+    }
+}
